059dee6542987630e3c31cd2335758f54c7f64fe
William Schrader <wfschrad@gmail.com>
2020-04-09 15:50:41 -0700

    add CRUD route handling and pug-template rendering

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const routes = require('./routes');
 
 const app = express();
+
 app.set('view engine', 'pug');
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.urlencoded( { extended: false }));
 app.use(routes);
 
-const port = 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.use((req, res, next) => {
+    const err = new Error('The requested page couldn\'t be found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if(process.env.NODE_ENV === 'production') {
+        // TODO Log the error to the database.
+    } else {
+        console.error(err);
+    }
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if(err.status === 404) {
+        res.status(404);
+        res.render('page-not-found', {
+            title: 'Page Not Found'            
+        })
+    }
+    else {
+        next(err)
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : err.message,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
+module.exports = app;
